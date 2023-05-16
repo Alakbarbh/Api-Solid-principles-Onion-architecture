7498e8c23d325ac67dcd0d9d52814f0678ca48ab
Alakbarbh <alakbarbh@code.edu.az>
2023-05-16 19:02:20 +0400

    Api, Solid principles, Onion architecture

---
+++ Api, Solid principles, Onion architecture/Api-intro/Api-intro/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_intro.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            string[] names = { "Eli", "Jale", "Pervin" };
+            return Ok(names);
+        }
+
+        //[HttpGet("GetEmployeeByTestId")] - spesifik çağırırıqsa bu qaydada çağırmalıyıq.Amma hər metod üçün yazmaq lazımdır.        
+        //[Route("GetEmployeeByTestId")] - və yaxud spesifik bu cürə
+        [HttpGet]
+        public IActionResult GetById(int? id)
+        {
+            if (id is null) return BadRequest();
+            return Ok("Eli - " + " " + id);
+        }
+    }
+}
